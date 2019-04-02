2b10c16cea99324db8cdac54616fa619b867070e
BlaCkinkGJ <ss5kijun@gmail.com>
2019-04-02 17:51:04 +0900

    Update the main.c file

--- main.c
+++ main.c
@@ -9,95 +9,52 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-/////// RELATED IN NEXT ASSIGNMENT FUNCTIONS //////
+struct postbox post;
+struct window_size screen_input_size = {
+    .height  = 5,
+    .width   = 52,
+    .start.x = 0,
+    .start.y = 19,
+};
+struct screen screen_input;
+
+struct window_size screen_output_size = {
+    .height  = 19,
+    .width   = 52,
+    .start.x = 0,
+    .start.y = 0,
+};
+struct screen screen_output;
+
+struct window_size screen_user_size = {
+    .height  = 24,
+    .width   = 28,
+    .start.x = (80-28),
+    .start.y = 0, // terminal width - user_size.width
+};
+struct screen screen_user;
+
+struct screen_raw_input input;
+
+void mainsetup();
+void mainloop();
+void mainend();
+
 void get_post(struct postbox* post, char **output, char **user);
 void put_post(struct postbox* post, char **output, char **user);
 
 int main(int argc, char *argv[]){
 
     ////// INITIALIZATION PART /////
-    struct postbox post; // This for next assignment
-
-    struct window_size screen_input_size = {
-        .height  = 5,
-        .width   = 52,
-        .start.x = 0,
-        .start.y = 19,
-    };
-    struct screen screen_input;
-
-    struct window_size screen_output_size = {
-        .height  = 19,
-        .width   = 52,
-        .start.x = 0,
-        .start.y = 0,
-    };
-    struct screen screen_output;
-
-    struct window_size screen_user_size = {
-        .height  = 24,
-        .width   = 28,
-        .start.x = (80-28),
-        .start.y = 0, // terminal width - user_size.width
-    };
-    struct screen screen_user;
-
-    struct screen_raw_input input;
-
-    if(argc > 1)
-        strcpy((char*)(input.user.name), argv[1]);
-    else
-        strcpy((char*)(input.user.name), "201424494");
-
-    initscr();
-    cbreak();
-
-    keypad(stdscr, TRUE);
-    refresh();
-    noecho();
-    curs_set(0);
-
-    screen_output = output_screen(screen_output_size);
-    screen_input  = input_screen(screen_input_size);
-    screen_user   = user_screen(screen_user_size);
-
-    screen_user.s_op->init(&screen_user);
-    screen_output.s_op->init(&screen_output);
-    screen_input.s_op->init(&screen_input);
-
-    screen_user.s_op->loop(&screen_user, (void *)(&input.user));
-
-    // BELOW LINE ONLY USE THIS ASSIGNMENT
-    put_post(&post, screen_output.buffer, screen_user.buffer);
+    mainsetup();
 
     ///// MAIN RUNNING LOOP /////
     while((input.data = getch()) != KEY_F(1)){
-        // 'get_post' is function for next assignment
-        get_post(&post, screen_output.buffer, screen_user.buffer);
-        if((char)input.data == '\n'){
-            // get message from input box
-            time_t now;
-            struct message *msg = (struct message *)malloc(sizeof(struct message));
-            time(&now);
-            msg->sender = input.user;
-            msg->timestamp = now;
-            memcpy(msg->data, screen_input.buffer, sizeof(msg->data));
-            // update the output screen
-            screen_output.s_op->loop(&screen_output, (void *)msg);
-            // clear the input screen
-            screen_input.s_op->loop(&screen_input, (void *)&input);
-            free(msg);
-        } else {
-            // update the input screen
-            screen_input.s_op->loop(&screen_input, (void *)&input);
-        }
-        // 'put_post' is function for next assignment
-        put_post(&post, screen_output.buffer, screen_user.buffer);
+        mainloop();
     }
 
-    endwin();
-    screen_output.s_op->close(&screen_output);
-    screen_input.s_op->close(&screen_input);
+    mainend();
+
     return 0;
 }
 
@@ -122,3 +79,62 @@ void put_post(struct postbox* post, char **output, char **user) {
         strcpy(post->user[index], user[index]);
     }
 }
+
+void mainsetup() {
+    initscr();
+    cbreak();
+
+    keypad(stdscr, TRUE);
+    refresh();
+    noecho();
+    curs_set(0);
+
+    screen_output = output_screen(screen_output_size);
+    screen_input  = input_screen(screen_input_size);
+    screen_user   = user_screen(screen_user_size);
+
+    screen_user.s_op->init(&screen_user);
+    screen_output.s_op->init(&screen_output);
+    screen_input.s_op->init(&screen_input);
+
+
+    if(argc > 1)
+        strcpy((char*)(input.user.name), argv[1]);
+    else
+        strcpy((char*)(input.user.name), "201424494");
+
+    screen_user.s_op->loop(&screen_user, (void *)(&input.user));
+    put_post(&post, screen_output.buffer, screen_user.buffer);
+}
+
+void mainloop() {
+    // 'get_post' is function for next assignment
+    get_post(&post, screen_output.buffer, screen_user.buffer);
+    if((char)input.data == '\n'){
+        // get message from input box
+        time_t now;
+        struct message *msg = (struct message *)malloc(sizeof(struct message));
+        time(&now);
+        msg->sender = input.user;
+        msg->timestamp = now;
+        memcpy(msg->data, screen_input.buffer, sizeof(msg->data));
+        // update the output screen
+        screen_output.s_op->loop(&screen_output, (void *)msg);
+        // clear the input screen
+        screen_input.s_op->loop(&screen_input, (void *)&input);
+        free(msg);
+    } else {
+        // update the input screen
+        screen_input.s_op->loop(&screen_input, (void *)&input);
+    }
+    // 'put_post' is function for next assignment
+    put_post(&post, screen_output.buffer, screen_user.buffer);
+}
+
+void mainend() {
+    endwin();
+    screen_output.s_op->close(&screen_output);
+    screen_input.s_op->close(&screen_input);
+    screen_user.s_op->close(&screen_input);
+}
+
